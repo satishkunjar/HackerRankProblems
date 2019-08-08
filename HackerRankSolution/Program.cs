6ff099514c5882488f66b49b44b955e4c402b7d7
satish kumar <sssatishkumar96@gmail.com>
2019-08-08 22:57:18 +0530

    orangeapple

--- HackerRankSolution/Program.cs
+++ HackerRankSolution/Program.cs
@@ -8,8 +8,62 @@ namespace HackerRankSolution
 {
     class Program
     {
+        // Complete the countApplesAndOranges function below.
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+
+            int applecount = 0;
+            int orangecount = 0;
+            foreach (int apple in apples)
+            {
+                if ((apple + a) >= s && (apple + a) <= t) {
+                    applecount++;
+                }
+            }
+
+            foreach (int orange in oranges)
+            {
+                if ((orange + b) >= s && (orange + b) <= t)
+                {
+                    orangecount++;
+                }
+            }
+            Console.WriteLine(applecount);
+            Console.WriteLine(orangecount);
+        }
+
         static void Main(string[] args)
         {
+            string[] st = Console.ReadLine().Split(' ');
+
+            int s = Convert.ToInt32(st[0]);
+
+            int t = Convert.ToInt32(st[1]);
+
+            string[] ab = Console.ReadLine().Split(' ');
+
+            int a = Convert.ToInt32(ab[0]);
+
+            int b = Convert.ToInt32(ab[1]);
+
+            string[] mn = Console.ReadLine().Split(' ');
+
+            int m = Convert.ToInt32(mn[0]);
+
+            int n = Convert.ToInt32(mn[1]);
+
+            int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+            ;
+
+            int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+            ;
+            countApplesAndOranges(s, t, a, b, apples, oranges);
         }
     }
 }
+//input test case
+//7 11
+//5 15
+//3 2
+//-2 2 1
+//5 -6
